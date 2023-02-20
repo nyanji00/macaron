e9b3257f85b34182b90ba729f42185d221dd0f4c
hyeonji-shin <hyeonji.shin@toss.im>
2023-02-21 00:19:05 +0900

    scaffold new nextjs project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>좋은 글 아카이브</title>
+        <meta name="description" content="좋은 글 아카이브" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
